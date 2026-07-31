80e8715c64895263e18a9b4a6fc6e6c95c3b7bd3
shubhamrana2662 <shubhamranasouli2007@gmail.com>
2026-07-31 14:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.24%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-       
-         int ans=0;
-       for (int i =0 ; i<word.size();i++){
-        ans +=i/8+1;
-       }
+        int x = word.size();
+         int ans;
+        if (x<=8)return x;
+        else if (x<=16&&x>8)
+        {
+             ans = 8+((x-8)*2);
+        }else if (x<=24&&x>16){
+             ans = 24+((x-16)*3);
+        }else {
+            ans =48+((x-24)*4);
+        }
         return ans;
     }
 };
