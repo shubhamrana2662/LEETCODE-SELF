6355bf5a93392e82639224a0949a55215edbce6b
shubhamrana2662 <shubhamranasouli2007@gmail.com>
2026-08-02 14:08:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (46.05%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-  vector<vector<int>> dp;
-     int fn(vector<int>& piles,int i , int j){
-      
-        if (i ==j){
-            return piles[i];
-        }
-        else if (dp[i][j]!=-1)return dp[i][j];
-        else {
-            int left = piles[i]-fn(piles,i+1,j);
-            int right = piles[j]-fn(piles,i,j-1);
-            dp[i][j] = max(left,right);
-
-                return dp[i][j];
-        }
-     }
-
-
-
-    
     bool stoneGame(vector<int>& piles) {
-    int n=piles.size();
-    dp.resize(n, vector<int>(n, -1));
-     int ans = fn(piles, 0, n - 1);
-
-      return ans >= 0;
-     
+        return true;
     }
 };
