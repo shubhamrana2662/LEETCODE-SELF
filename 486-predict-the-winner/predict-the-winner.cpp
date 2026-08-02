c3a34d03a48035bc8ecb50fb38f19ee78b72e9c2
shubhamrana2662 <shubhamranasouli2007@gmail.com>
2026-08-02 18:45:54 +0530

    Time: 2 ms (43.99%) | Memory: 10 MB (53.22%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-vector <vector<int>> v;
-
-    int cpa(vector<int>&nums,int l,int r){
-        if(l==r)return nums[l];
-        else if (v[l][r]!=-1)return v[l][r];
-        else{
-        int left=nums[l]-cpa(nums,l+1,r);
-        int right=nums[r]-cpa(nums,l,r-1);
-        v[l][r]=max(left,right);
-        return v[l][r];
+  vector<vector<int>> dp;
+     int fn(vector<int>& nums,int i , int j){
+      
+        if (i ==j){
+            return nums[i];
         }
+        else if (dp[i][j]!=-1)return dp[i][j];
+        else {
+            int left = nums[i]-fn(nums,i+1,j);
+            int right = nums[j]-fn(nums,i,j-1);
+            dp[i][j] = max(left,right);
 
-    }
+                return dp[i][j];
+        }
+     }
+
+
+
+    
     bool predictTheWinner(vector<int>& nums) {
-        v.resize(nums.size(), vector<int>(nums.size(), -1));
-        int p1=cpa(nums,0,nums.size()-1);
-        return p1>=0;
-        
+int n=nums.size();
+int ans;
+if (n%2==0)return true;
+else{
+dp.resize(n, vector<int>(n, -1));
+ ans = fn(nums, 0, n - 1);
+}
+return ans >= 0;
+     
     }
 };
