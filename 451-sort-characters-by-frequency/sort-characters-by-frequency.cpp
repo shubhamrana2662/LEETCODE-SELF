224aea2f8815b1cc6be950059194f87b474e1384
shubhamrana2662 <shubhamranasouli2007@gmail.com>
2026-08-06 23:55:21 +0530

    Time: 61 ms (5.30%) | Memory: 10.5 MB (93.88%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+
+        unordered_map<char,int> freq;
+
+        for(char c : s)
+            freq[c]++;
+
+        sort(s.begin(), s.end(), [&](char a, char b) {
+
+            if(freq[a] == freq[b])
+                return a < b;     
+
+            return freq[a] > freq[b];
+        });
+
+        return s;
+    }
+};
