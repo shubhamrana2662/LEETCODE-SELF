09db463602fa46e144a1ddb67b721ed0a9c8c8bb
shubhamrana2662 <shubhamranasouli2007@gmail.com>
2026-08-14 11:52:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (77.33%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -4,19 +4,17 @@ public:
     vector <int>arr(26,0);
     int left =0;
     int right =0;
-    int i=0;
+    
     int max1=0;
     while(right<s.size()){
        arr[s[right]-97]++;
-       if(arr[s[right]-97]<=2){
      
-       }else {
         while(arr[s[right]-97]>2){
       arr[s[left]-97]--;
       left++;
        }
 
-       }
+       
        right++;
       
 max1 = max(right-left,max1);
